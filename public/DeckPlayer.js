b381e22e3ee7a27dee5174e12c7a8f741b5e7c43
Tyler Tolley <thattolleyguy@gmail.com>
2019-02-10 21:46:30 -0700

    Fade between decks

--- public/DeckPlayer.js
+++ public/DeckPlayer.js
@@ -5,7 +5,7 @@ function DeckPlayer(elementId) {
     this.playerElement = document.getElementById(elementId);
     this.loadTrack = function (trackNum) {
         _this.playerElement.src = _this.playlist[trackNum].file;
-        document.getElementById('nowPlayingLabel').textContent=_this.playlist[trackNum].name;
+        document.getElementById('nowPlayingLabel').textContent = _this.playlist[trackNum].name;
     }
 
     this.playNext = function () {
@@ -32,5 +32,32 @@ function DeckPlayer(elementId) {
 
     this.playerElement.onended = function () {
         _this.playNext();
+    }
+
+    this.fadeIn = function () {
+        var distance = 1 - this.playerElement.volume;
+        var iterations = Math.ceil(distance / 0.01)
+        this.playerElement.play();
+        this.fade(.01, iterations);
+    }
+
+    this.fade = function (stepSize, iterations) {
+        if (iterations <= 1) {
+            if (stepSize < 0) {
+                this.playerElement.volume = 0;
+                this.playerElement.pause();
+            } else {
+                this.playerElement.volume = 1;
+            }
+        } else {
+            this.playerElement.volume += stepSize;
+            setTimeout(() => { this.fade(stepSize, iterations - 1) },10);
+        }
+    }
+
+    this.fadeOut = function () {
+        var distance = this.playerElement.volume;
+        var iterations = Math.ceil(distance / 0.01)
+        this.fade(-.01, iterations);
     }
 }

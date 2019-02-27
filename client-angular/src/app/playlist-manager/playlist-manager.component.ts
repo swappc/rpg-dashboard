a74f6d2277253a7f059b18adeaa77ee14e0eb501
gurfuzle <michael.swensen@domo.com>
2019-02-27 08:14:21 -0700

    Added creation for playlists.

--- client-angular/src/app/playlist-manager/playlist-manager.component.ts
+++ client-angular/src/app/playlist-manager/playlist-manager.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { Playlist } from '../playlist'
 import { MatOptionSelectionChange, MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
+import { PlaylistService } from '../playlist-service.service';
 
 export interface DialogData {
   name: string;
@@ -11,22 +13,39 @@ export interface DialogData {
   styleUrls: ['./playlist-manager.component.css']
 })
 export class PlaylistManagerComponent implements OnInit {
-  selected = 'option1';
-  playlists = [
-    {code: '1', name: 'Toby'},
-    {code: '2', name: 'Sarah'},
-    {code: '3', name: 'Frank'},
-    {code: '4', name: 'Leslie'},
-    {code: '5', name: 'Kyle'},
-    {code: '6', name: 'Megan'},
-    {code: '7', name: 'Rod'}
-  ]
+  selected = null;
+  playlists: Playlist[];
+  // playlists = [
+  //   {code: '1', name: 'Toby'},
+  //   {code: '2', name: 'Sarah'},
+  //   {code: '3', name: 'Frank'},
+  //   {code: '4', name: 'Leslie'},
+  //   {code: '5', name: 'Kyle'},
+  //   {code: '6', name: 'Megan'},
+  //   {code: '7', name: 'Rod'}
+  // ]
 
   name: string;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(
+    public dialog: MatDialog,
+    private playlistService: PlaylistService) { }
 
   ngOnInit() {
+    this.getPlaylists();
+  }
+
+  getPlaylists() {
+    this.playlistService.getPlaylists()
+      .subscribe(playlists => {
+        this.playlists = playlists;
+        this.playlists.forEach((element, index) => {
+          //TODO populate the track list.
+        });
+        if (playlists.length > 0) {
+          this.selected = this.playlists[0].name;
+        }
+      });
   }
 
   playlistChanged(event: MatOptionSelectionChange, playlist: any) {
@@ -37,14 +56,30 @@ export class PlaylistManagerComponent implements OnInit {
 
   newPlaylistClicked(): void {
     const dialogRef = this.dialog.open(PlaylistManagerNewPlaylistDialog, {
-    width: '250px',
-    data: {name: this.name}
-  });
+      width: '250px',
+      data: { name: this.name }
+    });
 
-  dialogRef.afterClosed().subscribe(result => {
-    console.log('The dialog was closed');
-    this.playlists.push({code: String(this.playlists.length + 1), name: result});
-  });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        var playlist = new Playlist();
+        playlist.name = result;
+        this.playlistService.createPlaylist(playlist)
+          .subscribe(playlist => {
+            this.playlists.push(playlist);
+            this.playlists.sort((a, b) => {
+              if (a.name > b.name) {
+                return 1;
+              }
+              if (a.name < b.name) {
+                return -1;
+              }
+              return 0;
+            });
+            this.selected = playlist.name;
+          });
+      }
+    });
   }
 
 }
@@ -56,7 +91,7 @@ export class PlaylistManagerComponent implements OnInit {
 export class PlaylistManagerNewPlaylistDialog {
   constructor(
     public dialogRef: MatDialogRef<PlaylistManagerNewPlaylistDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   onNoClick(): void {
     this.dialogRef.close();

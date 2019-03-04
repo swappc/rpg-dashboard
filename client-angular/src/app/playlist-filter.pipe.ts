c88e40ae7c8f26702c86ba5e4aa7a43c9a448cd9
gurfuzle <michael.swensen@domo.com>
2019-03-04 10:59:45 -0700

    Added mmissing files

---
+++ client-angular/src/app/playlist-filter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Playlist } from './playlist';
+
+@Pipe({
+  name: 'playlistFilter',
+  pure: false
+})
+export class PlaylistFilterPipe implements PipeTransform {
+
+  transform(items: Playlist[], searchText: string): any[] {
+      console.log("Searching For: " + searchText);
+    if (!items) return [];
+    if (!searchText) return items;
+    searchText = searchText.toLowerCase();
+    return items.filter(it => {
+      return it.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+  }
+}

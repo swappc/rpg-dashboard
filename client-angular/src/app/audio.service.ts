a1103872d5eca90fb0b5e759e876a4ca2a53940f
Tyler Tolley <thattolleyguy@gmail.com>
2019-03-05 15:51:56 -0700

    Break out playlist audio service.

---
+++ client-angular/src/app/audio.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { LibraryService, Crate } from './library.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AudioService {
+  
+}

1fae99b214bf5166a898f0cca159b52b7aedc29e
Tyler Tolley <thattolleyguy@gmail.com>
2019-03-05 17:02:28 -0700

    Create global midi service

--- client-angular/src/app/audio.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { LibraryService, Crate } from './library.service';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class AudioService {
-  
-}
